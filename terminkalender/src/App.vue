fce83496963c370333fb5b72da3ab49cdec8591a
DetlevBarthel <info@robo-studio.de>
2020-10-29 21:40:59 +0100

    terminkalender 1. commit

---
+++ terminkalender/src/App.vue
@@ -0,0 +1,111 @@
+<template>
+  <div id="app">
+    <div id="calender-week">
+      <div class="container-fluid mt-5">
+        <div class="row">
+          <div class="col-12">
+            <div class="card-group">
+              <!-- Tage -->
+              <div class="card">
+                <div class="card-header text-center bg-vue">
+                  <strong>Montag</strong>
+                </div>
+                <div class="card-body">
+                  <div>1</div>
+                  <div class="alert alert-success text-center">
+                    <div>Müll rausbringen</div>
+                    <div>
+                      <i class="fas fa-edit mr-2"></i>
+                      <i class="far fa-trash-alt"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="card">
+                <div class="card-header text-center bg-vue">
+                  <strong>Dienstag</strong>
+                </div>
+                <div class="card-body">
+                  <div>1</div>
+                </div>
+              </div>
+
+              <div class="card">
+                <div class="card-header text-center bg-vue">
+                  <strong>Mittwoch</strong>
+                </div>
+                <div class="card-body">
+                  <div>1</div>
+                </div>
+              </div>
+
+              <div class="card">
+                <div class="card-header text-center bg-vue">
+                  <strong>Donnerstag</strong>
+                </div>
+                <div class="card-body">
+                  <div>1</div>
+                </div>
+              </div>
+
+              <div class="card">
+                <div class="card-header text-center bg-vue">
+                  <strong>Freitag</strong>
+                </div>
+                <div class="card-body">
+                  <div>1</div>
+                  <div class="alert alert-success text-center">
+                    <div>Müll rausbringen</div>
+                    <div>
+                      <i class="fas fa-edit mr-2"></i>
+                      <i class="far fa-trash-alt"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="card">
+                <div class="card-header text-center bg-vue">
+                  <strong>Samstag</strong>
+                </div>
+                <div class="card-body">
+                  <div>1</div>
+                </div>
+              </div>
+
+              <div class="card">
+                <div class="card-header text-center bg-vue">
+                  <strong>Sonntag</strong>
+                </div>
+                <div class="card-body">
+                  <div>1</div>
+                  <div class="alert alert-primary text-center">
+                    <div>Grillen mit freunden</div>
+                    <div>
+                      <i class="fas fa-edit mr-2"></i>
+                      <i class="far fa-trash-alt"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="calender-entry"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+<style scoped>
+.bg-vue {
+  background-color: rgb(52, 73, 94);
+  color: white;
+}
+</style>
